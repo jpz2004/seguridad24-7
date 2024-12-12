6f98047a3629e8bcde06f887572e4c065979382a
jpz2004 <147892167+jpz2004@users.noreply.github.com>
2024-12-12 17:15:41 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('ingresar').addEventListener('click', () => {
-    window.location.href = 'login.html';
-});
-
-document.getElementById('registro').addEventListener('click', () => {
-    window.location.href = 'registro.html';
-});
